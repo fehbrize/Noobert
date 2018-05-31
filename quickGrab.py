046e7fe48fc0e3f289ad3e183f90f5fb1ca35749
Brianna <brifehring@gmail.com>
2018-05-31 17:33:52 -0400

    module complete

--- quickGrab.py
+++ quickGrab.py
@@ -2,10 +2,16 @@ from PIL import ImageGrab
 import time
 import os
 
+# Globals
+# --------------------------
+
+x_pad = 464
+y_pad = 243
+
 
 def screen_grab():
-    box = ()
-    im = ImageGrab.grab()
+    box = (x_pad + 1, y_pad + 1, x_pad + 640, y_pad + 480)
+    im = ImageGrab.grab(box)
     im.save(os.getcwd() + '\\full_snap__' + str(int(time.time())) + '.png', 'PNG')
 
 
